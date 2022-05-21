dc20bd2ed557c99c24436e45f34a2cc4f5a7bb55
MrHumble21 <malikovabdulboriy010@gmail.com>
2022-05-22 00:55:12 +0500

    styled navbar

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { html, css } from "./data";
 import Message from "./Message";
+import Navbar from "./Navbar";
 import Plans from "./Plans";
 
 function Main() {
   return (
     <>
+    <Navbar />
       <div className="container  mt-5">
         <Message />
       </div>
