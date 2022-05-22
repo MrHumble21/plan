55f17bfb7d2ce795d2e81482c13703e14ba65b84
MrHumble21 <malikovabdulboriy010@gmail.com>
2022-05-22 13:50:32 +0500

    boom

--- src/components/Lesson.jsx
+++ src/components/Lesson.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
-function Lesson() {
+function Lesson(props) {
   return (
-    <div>Lesson</div>
-  )
+    <div className="mt-5 container">
+      <div className="card border-info mb-3">
+        <div className="card-header">{props.name}</div>
+        <div className="card-body">
+          <h4 className="card-title">{props.name}</h4>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Lesson
+export default Lesson;
